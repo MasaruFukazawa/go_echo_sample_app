ea73c2ed05227d4ae251defa4cbd3e08a560ddce
Masaru Fukazawa <upw.ponsuke@gmail.com>
2025-10-30 22:44:13 +0900

    FirstCommite

---
+++ app/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello!!")
+	})
+
+	e.GET("/goodbey", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Goodbey!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
